59fa45014f8fd4908d19e346db405062dd742114
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-19 15:07:21 +0100

    fix bugs

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 
 port=$1
 
+if [ x$port = x ]; then
+  port=8000
+fi
+
 node ./local.js
 browserify client.js --debug -o static/bundle.js
 tacodb bundle index.js > bundle.js
-curl -X PUT -sSNT bundle.js localhost:$1/data/taco-demo
+curl -X PUT -sSNT bundle.js localhost:$port/data/taco-demo
 node update.js $port
 
